2dcc74f553e389fd492eea45ae24a89e1e81d68e
marufshafiq <marufshafiqcsecr@gmail.com>
2025-09-04 21:37:34 +0600

    Responsive Design

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const toggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
